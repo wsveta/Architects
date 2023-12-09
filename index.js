2a45f6a094c59f91cc4b85ff11cadf1012113bb2
Svitlana <wsv3ta@gmail.com>
2023-12-09 13:45:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: "#custom-pagination-container",
+      clickable: true,
+
+      type: "fraction",
+      formatFractionCurrent: function (number) {
+        return ("0" + number).slice(-2); // Додаємо "0" перед номером і обираємо останні дві цифри
+      },
+      formatFractionTotal: function (number) {
+        return ("0" + number).slice(-2); // Те ж саме для загальної кількості слайдів
+      },
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: "#custom-button-next",
+      prevEl: "#custom-button-prev",
+    },
+    parallax: true,
+    keyboard: {
+      enabled: true,
+    },
+  });
