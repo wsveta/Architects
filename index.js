d7a72c7f1d85851cc88f5aa17657db552b40b031
Svitlana <wsv3ta@gmail.com>
2023-12-09 15:54:00 +0100

    update 1

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 500,
+  effect: "fade",
 
-    // If we need pagination
-    pagination: {
-      el: "#custom-pagination-container",
-      clickable: true,
+  // If we need pagination
+  pagination: {
+    el: "#custom-pagination-container",
+    clickable: true,
 
-      type: "fraction",
-      formatFractionCurrent: function (number) {
-        return ("0" + number).slice(-2); // Додаємо "0" перед номером і обираємо останні дві цифри
-      },
-      formatFractionTotal: function (number) {
-        return ("0" + number).slice(-2); // Те ж саме для загальної кількості слайдів
-      },
+    type: "fraction",
+    formatFractionCurrent: function (number) {
+      return ("0" + number).slice(-2); // Додаємо "0" перед номером і обираємо останні дві цифри
     },
+    formatFractionTotal: function (number) {
+      return ("0" + number).slice(-2); // Те ж саме для загальної кількості слайдів
+    },
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: "#custom-button-next",
-      prevEl: "#custom-button-prev",
-    },
-    parallax: true,
-    keyboard: {
-      enabled: true,
-    },
-  });
+  // Navigation arrows
+  navigation: {
+    nextEl: "#custom-button-next",
+    prevEl: "#custom-button-prev",
+  },
+  parallax: true,
+  keyboard: {
+    enabled: true,
+  },
+});
